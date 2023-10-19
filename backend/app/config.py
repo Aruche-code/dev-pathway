498c895e5dc0635f13e40dc212c86c81dee936f3
Aruche-code <mo.crew2435@gmail.com>
2023-10-20 01:59:17 +0900

    環境変数として渡すための変更

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,4 +2,12 @@
 # このファイルは、アプリケーションの設定情報を管理します。
 # 環境に依存する変数や設定値を集約、
 # 接続情報。この情報は環境に依存し、安全に保管する必要があるため、環境変数から取得することが望ましい。
-DATABASE_URL = "postgresql://user:password@db/career_center"
+
+import os
+from dotenv import load_dotenv
+
+# .envファイルから環境変数を読み込む
+load_dotenv()
+
+# 環境変数を使用してデータベースURLを取得
+DATABASE_URL = os.getenv("DATABASE_URL")
