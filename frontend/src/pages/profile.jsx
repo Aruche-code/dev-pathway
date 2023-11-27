8c259f472b8018f1c5c5f48ce630b03320d86be1
Kanon74 <kd1320455@st.kobedenshi.ac.jp>
2023-11-27 13:30:29 +0900

    コメント削除

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -22,7 +22,6 @@ export default function profile() {
           </div>
           <div className="flex items-center mt-3 w-8/12">
             <label className="text-gray-600 w-1/3">性別</label>
-            {/* <input className="p-2 border-2 rounded-lg w-2/3 px-4" /> */}
             <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
             <label for="default-radio-1" class="ms-2 text-sm font-medium text-gray-900">男性</label>
             <input id="default-radio-2" type="radio" value="" name="default-radio2" className="ml-5 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
